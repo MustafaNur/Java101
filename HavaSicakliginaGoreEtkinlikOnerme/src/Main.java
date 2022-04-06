34486c1754eff0af09973183af6e4deb48564a93
Mustafa Nur <mustafa@drupart.co.uk>
2022-04-06 22:10:05 +0300

    etkinlik önerme
    hava sıcaklığına göre etkinlik önerme pratiği

---
+++ HavaSicakliginaGoreEtkinlikOnerme/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık giriniz: ");
+        heat = input.nextInt();
+
+        if (heat < 5){
+            System.out.print("Kayak yapabiilirsiniz");
+        }else if (heat <= 25){
+            if (heat <= 15){
+                System.out.print("Sinamaya gidebilirsiniz");
+            }
+            if (heat >= 10){
+                System.out.print("Pikniğe gidebilirsiniz");
+            }
+        }else {
+            System.out.print("Yüzmeye gidebilirsiniz");
+        }
+    }
+}
